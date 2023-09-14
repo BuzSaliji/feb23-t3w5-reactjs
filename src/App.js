16c50d1ae61d1dc932ecce9f57d000027cd90c86
Burim Saliji <buz.saliji@gmail.com>
2023-09-14 19:57:05 +1000

    feat: fruit list updated to use props data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
-// import some JSX element
+
+// Import custom JSX element:
 import FruitList from './FruitList';
 
-// some comment here
+// some comment here 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello world!</h1>
       <ul>
         {/* Array of li tags with fruit names as contents */}
-        {/* Use custom jsx element */}
-        <FruitList />
+        {/* Use custom JSX element: */}
+        <h1>Yellow fruits</h1>
+        <FruitList listToShow={["banana","mango","lemon"]} /> 
+
+        <h1>Green fruits</h1>
+        <FruitList listToShow={["apple","kiwi","lime"]} /> 
+
       </ul>
     </div>
   );
--- src/FruitList.jsx
+++ src/FruitList.jsx
@@ -20,13 +20,17 @@ let someFruit = [
 ]
   
 // Process someFruit and build an array of li tags 
-let fruitList = someFruit.map((fruit => {
-return <li>{fruit}</li>
+let listOfListTags = someFruit.map(((fruit, index) => {
+return <li key={fruit} fancyNumber={index} >{fruit}</li>
 }));
 
 
 // Function must return jsx. if you want it as HTML
 // aka in place of inline JSX
-export default function FruitList(){
-	return fruitList;
+export default function FruitList(props){
+	return props.listToShow.map(fruit => {
+		return <li key={fruit}>
+			{fruit}
+		</li>
+	});
 }
