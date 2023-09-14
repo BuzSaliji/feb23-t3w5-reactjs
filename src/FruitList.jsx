f7eb1975a68c4e567f6d082ae4baa135c795f1b1
Burim Saliji <buz.saliji@gmail.com>
2023-09-14 20:33:54 +1000

    feat: additional rendering of pineapple and children elements

--- src/FruitList.jsx
+++ src/FruitList.jsx
@@ -1,36 +1,49 @@
-let someFruit = [
-	"mangoes",
-	"peaches",
-	"apples",
-	"blueberries",
-	"aubergine",
-	"secret banana",
-	"papayas",
-	"banana",
-	"durian",
-	"tomatoes",
-	"rambutan",
-	"mulberries", 
-	"kiwis",
-	"jackfruit",
-	"watermelon",
-	"zach fruit",
-	"grape",
-	"coconut"
-]
+// Array of fruit names:
+// let someFruit = [
+// 	"mangoes",
+// 	"peaches",
+// 	"apples",
+// 	"blueberries",
+// 	"aubergine",
+// 	"secret banana",
+// 	"papayas",
+// 	"banana",
+// 	"durian",
+// 	"tomatoes",
+// 	"rambutan",
+// 	"mulberries", 
+// 	"kiwis",
+// 	"jackfruit",
+// 	"watermelon",
+// 	"zach fruit",
+// 	"grape",
+// 	"coconut"
+// ]
   
 // Process someFruit and build an array of li tags 
-let listOfListTags = someFruit.map(((fruit, index) => {
-return <li key={fruit} fancyNumber={index} >{fruit}</li>
-}));
+// let listOfListTags = someFruit.map(((fruit, index) => {
+// return <li key={fruit} fancyNumber={index} >
+// 	{fruit}
+// 	</li>
+// }));
 
-
-// Function must return jsx. if you want it as HTML
-// aka in place of inline JSX
+// Functions MUST return JSX, if you wanna use it as HTML
+// a.k.a in place of inline JSX:
 export default function FruitList(props){
-	return props.listToShow.map(fruit => {
+
+
+	if (props.listToShow.includes("pineapple")){
+		return null;
+	}
+
+	let listOfFruit = props.listToShow.map(fruit => {
 		return <li key={fruit}>
 			{fruit}
 		</li>
 	});
+
+	return <span>
+		{listOfFruit}
+		{props.children}
+	</span>
 }
